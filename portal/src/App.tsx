856b07023a7af4eeaded83db0bfb414ffdf0fe7d
Drake-Lundstrom <drakelundstrom@gmail.com>
2023-10-14 12:05:33 -0400

    added router

---
+++ portal/src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import { AppRouter } from './AppRouter'
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <AppRouter />
+      </header>
+    </div>
+  )
+}
+
+export default App
---
+++ portal/src/Pages/Home/HomePage.tsx
@@ -0,0 +1,5 @@
+export const HomePage = (): JSX.Element => {
+    return (
+      <h1>Home</h1>
+    );
+  };
