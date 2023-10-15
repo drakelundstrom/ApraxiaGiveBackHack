af5100d2abdc80598a9edb5f8404d6374b40aa37
Drake-Lundstrom <drakelundstrom@gmail.com>
2023-10-15 11:23:50 -0400

    added new design images

---
+++ portal/src/Pages/Decisions/DecisionsPage.tsx
@@ -0,0 +1,17 @@
+export const HomePage = (): JSX.Element => {
+	return (
+		<>
+			{/* <VerticalBox>
+				<HeaderBox>
+					<Header>Home</Header>
+					<ProfileImage src={picture} alt='profile picture' />
+				</HeaderBox>
+				<AppBox title='Step by Step' description='Breaking down overwelming challenges' redirectRoute={STEP_BY_STEP_ROUTE} picture={StepByStepIcon} />
+				<AppBox title='Pick One' description='Friendly help making decisions' redirectRoute={STEP_BY_STEP_ROUTE} picture={StepByStepIcon} />
+				<AppBox title='Magic 8 Ball' description='(Not yet implimented): Checking if purchases will break the bank' redirectRoute={''} picture={StepByStepIcon} />
+				<AppBox title='Food with Friends' description='(Not yet implimented): Finding restaurants that meet diet and sensory needs' redirectRoute={''} picture={StepByStepIcon} />
+				<AppBox title='Danger Will Robinson' description='(Not yet implimented): Automatic SOS call in overstimulating enviroments' redirectRoute={''} picture={StepByStepIcon} />
+			</VerticalBox> */}
+		</>
+	)
+}
